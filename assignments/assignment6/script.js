f902fa6c72b531928b806562a43b8730f3922625
Ryan Malone <ryan.malone192@yahoo.com>
2023-09-16 13:15:48 -0400

    Completed Assignment 6

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -11,19 +11,24 @@ const addComment = () => {
   const comment = document.getElementById("comment").value;
   const rating = document.getElementById("rating").value;
   const userName = document.getElementById("user-name").value;
-  
-  // Creates a div with the comment-box class, and adds the information from the constants above
+
+  // Creates a div element for the comment to go into
   const commentDiv = document.createElement("div");
   commentDiv.classList.add("comment-box");
-  commentDiv.innerHTML = '<h3>' + pName + '</h3> <br> <p>' + rating +' ' + rating + ' ' + comment + ' </p> <br> <p> by ' + userName + ' </p>';
+  commentDiv.innerHTML = '<h3>' + pName + '</h3> <br> <p>' + rating +'/5 stars ' + comment + ' </p> <br> <p> by ' + userName + ' </p>';
 
   // Adds the commentDiv to the comment-section section
   const commentSection = document.getElementById("comment-section");
   commentSection.appendChild(commentDiv);
 }
 
+const animate = () => {
+  document.getElementById("symbol").classList.add("animate");
+}
+
 window.onload = () => {
   document.getElementById("button-show").onclick = showImage;
   document.getElementById("button-hide").onclick = hideImage;
+  document.getElementById("button-dance").onclick = animate;
   document.getElementById("button-comment").onclick = addComment;
 }
