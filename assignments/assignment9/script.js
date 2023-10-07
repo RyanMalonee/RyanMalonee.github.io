2fcdaaf0e62a1979e122d3a24908594e4d1b38d6
Ryan Malone <ryan.malone192@yahoo.com>
2023-10-07 12:05:50 -0400

    Corrected indentation

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -2,11 +2,11 @@
 const switchQuotes = () => {
   let i = 0;
   const quotes = ["\"You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.\" - John Lennon",
-    "\"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.\" - Oprah Winfrey",
-    "\"The only thing we have to fear is fear itself.\" - Franklin D. Roosevelt",
-    "\"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.\" - Helen Keller",
-    "\"Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.\" - Martin Luther King Jr."]
-
+  "\"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.\" - Oprah Winfrey",
+  "\"The only thing we have to fear is fear itself.\" - Franklin D. Roosevelt",
+  "\"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.\" - Helen Keller",
+  "\"Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.\" - Martin Luther King Jr."]
+  
   const quoteContainer = document.getElementById("quote-container");
   quoteContainer.innerHTML = quotes[0];
   const changeQuote = setInterval(() => {
@@ -17,7 +17,7 @@ const switchQuotes = () => {
     }
     quoteContainer.innerHTML = quotes[i]; 
   }, 2000);
-
+  
 }
 
 const buildRainbow = () => {
@@ -26,23 +26,23 @@ const buildRainbow = () => {
   rainbowContainer.innerHTML = "";
   document.getElementById("pot-of-gold").classList.add("hide");
   let i = 0;
-    const drawRow = () => {
-      const paragraph = document.createElement("p");
-      paragraph.innerHTML = "hi";
-      rainbowContainer.appendChild(paragraph);
-      paragraph.style.backgroundColor = colors[i];
-      i++
-      // I used recursion until all rows are drawn - when all rows are drawn,
-      // the pot of gold is shown.
-      if(i < colors.length) {
-        setTimeout(drawRow, 1000);
-      } else {
-        setTimeout(() => {
-          document.getElementById("pot-of-gold").classList.remove("hide");
-        }, 1000);
-      }
+  const drawRow = () => {
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = "hi";
+    rainbowContainer.appendChild(paragraph);
+    paragraph.style.backgroundColor = colors[i];
+    i++
+    // I used recursion until all rows are drawn - when all rows are drawn,
+    // the pot of gold is shown.
+    if(i < colors.length) {
+      setTimeout(drawRow, 1000);
+    } else {
+      setTimeout(() => {
+        document.getElementById("pot-of-gold").classList.remove("hide");
+      }, 1000);
     }
-    drawRow();
+  }
+  drawRow();
 }
 
 window.onload = () => {
