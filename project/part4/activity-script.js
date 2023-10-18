c24edd1d9bc6815d802ea3800c8a1383b76759f4
Ryan Malone <ryan.malone192@gmail.com>
2023-10-17 22:00:11 -0400

    Added forms and iframe to site. Everything is working correctly

--- project/part4/activity-script.js
+++ project/part4/activity-script.js
@@ -5,10 +5,12 @@ const imageBoxContainer = () => {
   const background = document.getElementById("modal-background");
   const imageContainer = document.getElementById("image-container");
   const bigImage = document.getElementById("big-image");
+  const map = document.getElementById("map");
   const attribution = document.getElementById("attribution");
   document.querySelectorAll("main img").forEach((img) => {
     img.onclick = () => {
       bigImage.src = img.getAttribute("large-src");
+      map.src = img.getAttribute("map-src");
       background.classList.remove("hide");
       imageContainer.classList.add("show-image");
       attribution.innerHTML = img.getAttribute("att");
@@ -23,27 +25,60 @@ const imageBoxContainer = () => {
   document.querySelector(".x-button").onclick = () => {
     background.classList.add("hide");
     imageContainer.style.opacity = 0;
-  }
+  };
 
   window.onclick = (event) => {
-    if(event.target == background) {
+    if (event.target == background) {
       background.classList.add("hide");
       imageContainer.style.opacity = 0;
     }
-  }
-}
+  };
+};
 
-const formSubmitMessage = () => {
+const formSubmitMessage = (e) => {
+  e.preventDefault();
+  const formResultContainer = document.getElementById(
+    "new-recommendation-results"
+  );
   const responseMessage = document.createElement("p");
-  
-}
+  formResultContainer.innerHTML = "";
+
+  const name = document.getElementById("name-of-location").value;
+  const type = document.getElementById("type-of-location").value;
+  const address = document.getElementById("address").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
+  const openTime = document.getElementById("open-time").value;
+  const closeTime = document.getElementById("close-time").value;
+  const review = document.getElementById("review").value;
+  const description = document.getElementById("description").value;
+
+  responseMessage.innerHTML = `<div id="new-rec-result-container"> <h3>Thank you for your submission!</h3> <h4>Information:</h4>
+  <div class=flex-container> <section class="column-split">
+  <p>Name: ${name}</p>
+  <p>Type: ${type}</p>
+  <p>Address: ${address}</p>
+  </section>
+  <section class="column-split">
+  <p>Phone: ${phone}</p>
+  <p>Email: ${email}</p>
+  <p>Hours of Operation: ${openTime} - ${closeTime}</p>
+  </section>
+  <section class="column-split">
+  <p>Review: ${review}</p>
+  <p>Description: ${description}</p>
+  </section>
+  </div></div>`;
+  formResultContainer.appendChild(responseMessage);
+};
 
 const toggleHamburger = () => {
   document.getElementById("nav-items").classList.toggle("hide");
-}
+};
 
 window.onload = () => {
   document.getElementById("hamburger").onclick = toggleHamburger;
   imageBoxContainer();
-  document.getElementById("form-submit-button").onclick = formSubmitMessage;
-}
+  document.getElementById("new-recommendation-form").onsubmit =
+    formSubmitMessage;
+};
